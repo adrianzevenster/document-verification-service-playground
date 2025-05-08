909b7084e477d76c40f9581b3e82c374c525f63a
adrianzevenster <a.zevenstar@gmail.com>
2025-05-08 19:56:44 +0200

    module output added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@ output "assigned_roles" {
 output "bound_members" {
   value = var.members
 }
+
+output "bucket_name" {
+  value = module.gcs_bucket.bucket_name
+}
+
+output "processor_id" {
+  value = module.documentai_processor.processor_id
+}
+
+output "service_account_email" {
+  value = module.service_accounts.service_account_email
+}
