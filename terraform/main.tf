6f372b84d0a3ee77a042c6031da5fb774efd451b
adrianzevenster <a.zevenstar@gmail.com>
2025-05-08 19:28:51 +0200

    import modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 locals {
   roles = [
   "roles/documentai.apiUser",
@@ -11,6 +16,24 @@ locals {
   "roles/monitoring.viewer"]
 }
 
+module "gcs_bucket" {
+  source      = "./modules/gcs"
+  bucket_name = var.bucket_name
+}
+
+module "documentai_processor" {
+  source         = "./modules/documentai"
+  project_id     = var.project_id
+  location       = var.region
+  processor_name = var.processor_name
+}
+
+moduel "service_accounts" {
+  source               = "./modules/service_accounts"
+  project_id           = var.project_id
+  service_account_name = var.service_account_name
+}
+
 resource "google_project_iam_binding" "project_roles" {
   for_each = toset(local.roles)
 
@@ -19,3 +42,4 @@ resource "google_project_iam_binding" "project_roles" {
 
   members  = var.members
 }
+
