1a516a0a9c3e66f247ccfeec5392b079dfa7a9ee
adrianzevenster <a.zevenstar@gmail.com>
2025-05-08 19:29:26 +0200

    updated vars with modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,18 @@ variable "members" {
   description = "List of users or service accounts to bind roles to"
   type        = list(string)
 }
+
+variable "bucket_name" {
+  description = "Bucket to assign to objects"
+  type        = string
+}
+
+variable "processor_name" {
+  description = "Docai Processor Name"
+  type        = string
+}
+
+variable "service_account_name" {
+  description = "service accounts"
+  type        = string
+}
